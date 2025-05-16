c01adf95a3a1cdfc3bb49910bbe665ed454ebb8b
samar krishna <samar.krishna@techvyassa.com>
2025-05-16 16:15:23 +0530

    migrate

--- database/migrations/2025_05_06_add_so_no_to_pickings_table.php
+++ database/migrations/2025_05_06_add_so_no_to_pickings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSoNoToPickingsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -65,4 +65,4 @@ class AddSoNoToPickingsTable extends Migration
     {
         // Don't drop any columns in down method for safety
     }
-}
+};
