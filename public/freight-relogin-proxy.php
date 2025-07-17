4c4a9453447f02ad3a308a63859fa90c09b05228
samar krishna <samar.krishna@techvyassa.com>
2025-07-17 13:12:26 +0530

    relogi

--- public/freight-relogin-proxy.php
+++ public/freight-relogin-proxy.php
@@ -1,17 +1,29 @@
 <?php
-// public/freight-relogin-proxy.php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 $url = "http://ec2-52-205-180-161.compute-1.amazonaws.com/agro-api/delhivery-relogin";
 
-$options = [
-    "http" => [
-        "header"  => "Content-type: application/x-www-form-urlencoded\r\n",
-        "method"  => "POST",
-        "content" => file_get_contents("php://input"),
-    ],
-];
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
+// Get POST data
+$postData = file_get_contents("php://input");
 
-header('Content-Type: application/json');
-echo $result; 
+// Initialize cURL
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Content-Type: application/x-www-form-urlencoded"
+]);
+
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    http_response_code(500);
+    echo json_encode(["error" => curl_error($ch)]);
+} else {
+    header('Content-Type: application/json');
+    echo $response;
+}
+
+curl_close($ch); 
